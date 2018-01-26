dc9d3e7bed33e61db0ee2a2476bb59884800da72
臧天才 <laidu823@sina.com>
2018-01-26 19:07:23 +0800

    sync

--- app/src/main/java/org/laidu/android/reverse/hook/StringConstructorHook.java
+++ app/src/main/java/org/laidu/android/reverse/hook/StringConstructorHook.java
@@ -1,28 +1,28 @@
-package org.laidu.android.reverse.hook;
-
-import org.laidu.android.reverse.annotation.HookService;
-import org.laidu.android.reverse.util.MethodHookUtil;
-
-import java.util.Arrays;
-import java.util.List;
-
-/**
- * Created by laidu on 18-1-17.
- */
-
-@HookService(ignore = true)
-public class StringConstructorHook implements Hook {
-
-    @Override
-    public List<String> buildHookClassList() {
-        return Arrays.asList("java.lang.StringFactory",
-                "java.lang.String",
-                "java.lang.StringBuffer",
-                "java.lang.StringBuilder");
-    }
-
-    @Override
-    public void hook() {
-        buildHookClassList().forEach(MethodHookUtil::hookAllConstructor);
-    }
-}
+//package org.laidu.android.reverse.hook;
+//
+//import org.laidu.android.reverse.annotation.HookService;
+//import org.laidu.android.reverse.util.MethodHookUtil;
+//
+//import java.util.Arrays;
+//import java.util.List;
+//
+///**
+// * Created by laidu on 18-1-17.
+// */
+//
+//@HookService(ignore = true)
+//public class StringConstructorHook implements Hook {
+//
+//    @Override
+//    public List<String> buildHookClassList() {
+//        return Arrays.asList("java.lang.StringFactory",
+//                "java.lang.String",
+//                "java.lang.StringBuffer",
+//                "java.lang.StringBuilder");
+//    }
+//
+//    @Override
+//    public void hook() {
+//        buildHookClassList().forEach(MethodHookUtil::hookAllConstructor);
+//    }
+//}
